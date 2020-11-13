76d2326228ea69ad4b74856e5c6bbc9ac24b671a
valhuber <valjhuber@gmail.com>
2020-11-12 19:09:52 -0800

    Allocation class

--- logic_bank/extensions/allocate.py
+++ logic_bank/extensions/allocate.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 from typing import Callable
 
 import sqlalchemy
@@ -9,26 +10,67 @@ from logic_bank.exec_row_logic.logic_row import LogicRow
 from logic_bank.rule_bank import rule_bank_withdraw
 
 
-def allocate(from_provider_row: LogicRow,   # eg, payment
-             to_recipients: list,           # eg, unpaid orders
-             creating_allocation: object,   # PaymentAllocation (intersection)
-             while_calling_allocator: Callable):
-    for each_recipient in to_recipients:
-        new_allocation = creating_allocation()
-        new_allocation_logic_row = LogicRow(row=new_allocation, old_row=new_allocation,
-                                            ins_upd_dlt="ins",
-                                            nest_level=from_provider_row.nest_level+1,
-                                            a_session=from_provider_row.session,
-                                            row_sets=from_provider_row.row_sets)
-        new_allocation_logic_row.link(to_parent=from_provider_row)
-        recipient_mapper = get_mapper(each_recipient)
-        each_recipient_logic_row = LogicRow(row=each_recipient, old_row=each_recipient,
-                                            ins_upd_dlt="*", nest_level=0,
-                                            a_session=from_provider_row.session,
-                                            row_sets=None)
-        new_allocation_logic_row.link(to_parent=each_recipient_logic_row)
-        allocator = while_calling_allocator(new_allocation_logic_row, from_provider_row)
-        new_allocation_logic_row.update(reason="Allocate " + from_provider_row.name)
-        if not allocator:
-            break
-    print("Done")
+class Allocate():
+    """
+    Allocates anAmount from a Provider to Recipients, creating Allocation rows
+    @see https://github.com/valhuber/LogicBank/wiki/Sample-Project---Allocation
+    """
+
+    def __init__(self,
+                 from_provider_row: LogicRow,  # eg, payment
+                 to_recipients: list,          # eg, unpaid orders
+                 creating_allocation: object,  # eg, PaymentAllocation (junction)
+                 while_calling_allocator: Callable = None):
+        self.from_provider_row = from_provider_row
+        self.to_recipients = to_recipients
+        self.creating_allocation = creating_allocation
+        self.while_calling_allocator = while_calling_allocator
+
+    def execute(self):
+        """
+        Create allocation row for each recipient until while_calling_allocator returns false
+
+        :return:
+        """
+        for each_recipient in self.to_recipients:
+            new_allocation = self.creating_allocation()
+            new_allocation_logic_row = LogicRow(row=new_allocation, old_row=new_allocation,
+                                                ins_upd_dlt="ins",
+                                                nest_level=self.from_provider_row.nest_level + 1,
+                                                a_session=self.from_provider_row.session,
+                                                row_sets=self.from_provider_row.row_sets)
+            new_allocation_logic_row.link(to_parent=self.from_provider_row)
+            each_recipient_logic_row = LogicRow(row=each_recipient, old_row=each_recipient,
+                                                ins_upd_dlt="*", nest_level=0,
+                                                a_session=self.from_provider_row.session,
+                                                row_sets=None)
+            new_allocation_logic_row.link(to_parent=each_recipient_logic_row)
+            if self.while_calling_allocator is not None:
+                allocator = self.while_calling_allocator(new_allocation_logic_row, self.from_provider_row)
+            else:
+                allocator = self.each_allocation(new_allocation_logic_row, self.from_provider_row)
+            new_allocation_logic_row.insert(reason="Allocate " + self.from_provider_row.name)
+            if not allocator:
+                break
+        return self
+
+    def each_allocation(self, allocation_logic_row, provider_logic_row) -> bool:
+        """
+        Called for each created allocation,
+        to compute allocation.amount_allocated and reduce provider.AmountUnAllocated
+
+        This uses default names; to use your names, copy this code and alter as as required
+
+        :param allocation_logic_row: allocation row being created
+        :param provider_logic_row: provider
+        :return: provider has AmountUnAllocated remaining
+        """
+        if provider_logic_row.row.AmountUnAllocated is None:
+            provider_logic_row.row.AmountUnAllocated = provider_logic_row.row.Amount
+        amount = min(Decimal(provider_logic_row.row.AmountUnAllocated),
+                     Decimal(allocation_logic_row.row.Order.AmountOwed))
+        provider_logic_row.row.AmountUnAllocated = \
+            provider_logic_row.row.AmountUnAllocated - amount
+        allocation_logic_row.row.AmountAllocated = amount
+        any_left = provider_logic_row.row.AmountUnAllocated > 0
+        return any_left
--- payment_allocation/logic/rules_bank.py
+++ payment_allocation/logic/rules_bank.py
@@ -1,33 +1,20 @@
 from decimal import Decimal
 
 from logic_bank.exec_row_logic.logic_row import LogicRow
-from logic_bank.extensions.allocate import allocate
+from logic_bank.extensions.allocate import allocate, Allocate
 from logic_bank.logic_bank import Rule
 from payment_allocation.db.models import Customer, Order, Payment, PaymentAllocation
 
-def allocate_payment(row: Payment, old_row: Payment, logic_row: LogicRow):
-    def each_payment_allocation(allocation_logic_row, provider_logic_row):
-        if provider_logic_row.row.AmountUnAllocated is None:
-            provider_logic_row.row.AmountUnAllocated = provider_logic_row.row.Amount
-        amount = min(Decimal(provider_logic_row.row.AmountUnAllocated),
-                     Decimal(allocation_logic_row.row.Order.AmountOwed))
-        provider_logic_row.row.AmountUnAllocated = \
-            provider_logic_row.row.AmountUnAllocated - amount
-        allocation_logic_row.row.AmountAllocated = amount
-        any_left = provider_logic_row.row.AmountUnAllocated > 0
-        return any_left
 
-    # unpaid_orders = row.Customer.OrderList
-    # https://stackoverflow.com/questions/40524749/sqlalchemy-query-filter-on-child-attribute
-    # q = s.query(Parent).filter(Parent.child.has(Child.value > 20))
-    test_cust = row.Customer
+def allocate_payment(row: Payment, old_row: Payment, logic_row: LogicRow):
+    """ get unpaid orders (recipient), invoke allocation """
+    customer_of_payment = row.Customer
     unpaid_orders = logic_row.session.query(Order)\
-        .filter(Order.AmountOwed > 0, Order.CustomerId == test_cust.Id)\
+        .filter(Order.AmountOwed > 0, Order.CustomerId == customer_of_payment.Id)\
         .order_by(Order.OrderDate).all()
-    allocate(from_provider_row=logic_row,
+    Allocate(from_provider_row=logic_row,  # uses default while_calling_allocator
              to_recipients=unpaid_orders,
-             creating_allocation=PaymentAllocation,
-             while_calling_allocator=each_payment_allocation)
+             creating_allocation=PaymentAllocation).execute()
 
 
 def declare_logic():
@@ -41,6 +28,12 @@ def declare_logic():
     Rule.early_row_event(on_class=Payment, calling=allocate_payment)
 
     """
+    failed getting unpaid orders like this
+        https://stackoverflow.com/questions/40524749/sqlalchemy-query-filter-on-child-attribute
+        q = s.query(Parent).filter(Parent.child.has(Child.value > 20))
+    
+    sample data:
+
         (10653 owes nothing)
         orderId OrderDate   AmountTotal AmountPaid  AmountOwed  ==> Allocated
         10692   2013-10-03  878         0           100         100
