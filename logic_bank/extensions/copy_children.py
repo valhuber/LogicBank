fe7330d071007009fcb62c7b7b095442011cd378
valhuber <valjhuber@gmail.com>
2022-04-19 12:11:09 -0700

    1.06.00 - copy rule

---
+++ logic_bank/extensions/copy_children.py
@@ -0,0 +1,39 @@
+from decimal import Decimal
+from typing import Callable
+from logic_bank.exec_row_logic.logic_row import LogicRow
+from logic_bank.rule_type.row_event import RowEvent
+
+
+class CopyChildren(RowEvent):
+    """
+    Copy copy_from -> copy_to (e.g., auditing).
+    """
+    def __init__(self,
+                 copy_from: object,
+                 copy_to: object,  # eg, PaymentAllocation (junction)
+                 which_children: dict):
+        self.copy_to = copy_to
+        self.copy_from = copy_from
+        self.which_children = which_children
+        if copy_to is None:
+            raise Exception("copy_to object is required")
+        super(CopyChildren, self).__init__(copy_from, None)
+
+    def __str__(self):
+        copy_to = str(self.copy_to)
+        nodes = copy_to.split('.')
+        last_node = nodes[len(nodes)-1]
+        last_node = last_node[0: len(last_node)-2]
+        return f'Copy children: {last_node}'
+
+    def execute(self, logic_row: LogicRow):
+        """
+        Called by logic engine, overriding generic EarlyEvent rule.
+
+        Delegates to LogicRow.copy_children
+
+        :return:
+        """
+        copy_to = logic_row
+        copy_to.copy_children(copy_from=self.copy_from, which_children=self.which_children)
+        return self
