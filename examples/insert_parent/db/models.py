929c6cd9946b1de2ea028145aa34f2d3e3c25ec4
Val Huber <valjhuber@gmail.com>
2023-10-26 15:42:06 -0700

    1.09.00 - prelim version of insert_parent, for testing

---
+++ examples/insert_parent/db/models.py
@@ -0,0 +1,74 @@
+# coding: utf-8
+
+from logic_bank import logic_bank  # import this first - import ordering
+
+# import sqlalchemy_utils
+from sqlalchemy import Boolean, Column, DECIMAL, DateTime, Float, ForeignKey, Integer, LargeBinary, String, \
+    UniqueConstraint, select, func, ForeignKeyConstraint
+from sqlalchemy.orm import relationship, column_property
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.testing import db
+from sqlalchemy.ext.hybrid import hybrid_property, hybrid_method
+
+Base = declarative_base()
+metadata = Base.metadata
+
+
+class Parent(Base):
+    """
+    https://docs.sqlalchemy.org/en/14/orm/cascades.html
+    The default behavior of cascade is limited to cascades of the so-called
+    save-update and merge settings.
+    The typical “alternative” setting for cascade is to
+    add the delete and delete-orphan options;
+    these settings are appropriate for related objects which only exist
+    as long as they are attached to their parent, and are otherwise deleted.
+
+    #unitofwork-cascades.
+    The all symbol is a synonym for
+    save-update, merge, refresh-expire, expunge, delete,
+    and using it in conjunction with delete-orphan indicates that
+    the child object should follow along with its parent in all cases,
+    and be deleted once it is no longer associated with that parent.
+    """
+    __tablename__ = 'Parent'
+
+    parent_attr_1 = Column(String(16), primary_key=True)
+    parent_attr_2 = Column(String(16), primary_key=True)
+
+    ChildList = relationship("Child"
+                             , backref="Parent"
+                             , cascade="all"  # cascade delete
+                             # , passive_deletes=True  use *only* when DBMS does the cascade delete
+                             # for LogicBank delete logic
+                             , cascade_backrefs=True
+                             )
+    ChildOrphanList = relationship("ChildOrphan"
+                                   , backref="Parent"
+                                   , cascade="save-update, merge, refresh-expire, expunge"
+                                   # no delete option means "nullify"
+                                   , cascade_backrefs=True
+                                   )
+
+
+class Child(Base):
+    __tablename__ = 'Child'
+
+    child_key = Column(String(16), primary_key=True)
+    parent_1 = Column(String(16))
+    parent_2 = Column(String(16))
+    __table_args__ = (ForeignKeyConstraint([parent_1, parent_2],
+                                           [Parent.parent_attr_1, Parent.parent_attr_2]),
+                      {})
+
+
+
+class ChildOrphan(Base):
+    __tablename__ = 'ChildOrphan'
+
+    child_key = Column(String(16), primary_key=True)
+    parent_1 = Column(String(16))
+    parent_2 = Column(String(16))
+    __table_args__ = (ForeignKeyConstraint([parent_1, parent_2],
+                                           [Parent.parent_attr_1, Parent.parent_attr_2]),
+                      {})
---
+++ examples/insert_parent/tests/test_insert_parent.py
@@ -0,0 +1,233 @@
+from decimal import Decimal
+import logging, sys, os
+from shutil import copyfile
+
+import sqlalchemy
+
+from logic_bank_utils import util as logic_bank_utils
+
+(did_fix_path, sys_env_info) = \
+    logic_bank_utils.add_python_path(project_dir="LogicBank", my_file=__file__)
+
+from logic_bank.rule_bank import rule_bank_withdraw  # TODO fails
+from logic_bank.rule_type.parent_check import ParentCheck
+
+print("\n" + did_fix_path + "\n\n" + sys_env_info + "\n\n")
+
+from logic_bank.logic_bank import LogicBank
+from logic_bank.util import row_prt, prt, ConstraintException
+from logic_bank.exec_row_logic.logic_row import LogicRow
+
+import examples.referential_integrity.db.models as models
+
+def copy_db_from_gold():
+    """ copy db/database-gold.db over db/database.db"""
+    print("\n" + prt("restoring database-gold\n"))
+
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    basedir = os.path.dirname(basedir)
+
+    print("\n********************************\n"
+          "  IMPORTANT - create database.db from database-gold.db in " + basedir + "/payment_allocation/db/\n" +
+          "            - from -- " + prt("") +
+          "\n********************************")
+
+    db_loc = os.path.join(basedir, "db/database.db")
+    db_source = os.path.join(basedir, "db/database-gold.db")
+    copyfile(src=db_source, dst=db_loc)
+
+def setup_logging():
+    logic_logger = logging.getLogger('logic_logger')  # for debugging user logic
+    logic_logger.setLevel(logging.DEBUG)
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(message)s - %(asctime)s - %(name)s - %(levelname)s')
+    handler.setFormatter(formatter)
+    logic_logger.addHandler(handler)
+
+    do_engine_logging = False
+    engine_logger = logging.getLogger('engine_logger')  # for internals
+    if do_engine_logging:
+        engine_logger.setLevel(logging.DEBUG)
+        handler = logging.StreamHandler(sys.stdout)
+        handler.setLevel(logging.DEBUG)
+        formatter = logging.Formatter('%(message)s - %(asctime)s - %(name)s - %(levelname)s')
+        handler.setFormatter(formatter)
+        engine_logger.addHandler(handler)
+
+setup_logging()
+copy_db_from_gold()
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+basedir = os.path.dirname(basedir)
+
+db_loc = os.path.join(basedir, "db/database.db")
+
+conn_string = "sqlite:///" + db_loc
+engine = sqlalchemy.create_engine(conn_string, echo=False)  # sqlalchemy sqls...
+
+session_maker = sqlalchemy.orm.sessionmaker()
+session_maker.configure(bind=engine)
+session = session_maker()
+
+from examples.referential_integrity.logic.rules_bank import declare_logic
+LogicBank.activate(session=session, activator=declare_logic)
+
+pre_parent = session.query(models.Parent).filter(models.Parent.parent_attr_1 == "p1_1",
+                                                 models.Parent.parent_attr_2 == "p1_2").one()
+pre_parent_logic_row = LogicRow(row=pre_parent, old_row=pre_parent, ins_upd_dlt="*", nest_level=0, a_session=session, row_sets=None)
+children = pre_parent.ChildList
+session.expunge(pre_parent)
+
+pre_child = session.query(models.Child).filter(models.Child.child_key == "c1.1").one()
+pre_child_logic_row = LogicRow(row=pre_child, old_row=pre_child, ins_upd_dlt="*", nest_level=0, a_session=session, row_sets=None)
+session.expunge(pre_child)
+
+list_ref_integ_rules = rule_bank_withdraw.rules_of_class(pre_child_logic_row, ParentCheck)
+ref_integ_rule = list_ref_integ_rules[0]
+
+"""
+    Test 1 - insert child row with invalid key, verify fails
+"""
+print("\nBegin Test 1 - insert child row with invalid key, verify fails")
+new_child = models.Child(parent_1="xx", parent_2="yy", child_key="new child")
+
+session.add(new_child)
+did_fail_as_expected = False
+try:
+    session.commit()
+except ConstraintException as ce:
+    session.rollback()
+    assert ref_integ_rule._enable, "Ref Integ disabled, but raised"
+    did_fail_as_expected = True
+    print("Expected constraint caught: " + str(ce))
+except:
+    session.rollback()
+    did_fail_as_expected = False
+    e = sys.exc_info()[0]
+    print(e)
+
+if ref_integ_rule._enable:
+    assert did_fail_as_expected, "Test 1 failed: Ref Integ enabled, invalid FK did not raise ConstraintException"
+
+print("\n" + prt("Invalid parent failed as expected.  Now trying update..."))
+
+
+"""
+    Test 2 - update child row with invalid key, verify fails
+"""
+print("\nBegin Test 2 - update child row with invalid key, verify fails")
+child = session.query(models.Child).filter(models.Child.child_key == "c1.1").one()
+child.parent_1 = "Make Me Fail"
+did_fail_as_expected = False
+try:
+    session.commit()
+except ConstraintException as ce:
+    session.rollback()
+    assert ref_integ_rule._enable, "Ref Integ disabled, but raised"
+    did_fail_as_expected = True
+    print("Expected constraint caught: " + str(ce))
+except:
+    session.rollback()
+    did_fail_as_expected = False
+    e = sys.exc_info()[0]
+    print(e)
+
+if ref_integ_rule._enable:
+    assert did_fail_as_expected, "Test 2 failed: Ref Integ enabled, invalid FK did not raise ConstraintException"
+
+print("\n" + prt("Invalid parent failed as expected.  Now trying null..."))
+
+
+print("\nref_integ_tests, update completed\n\n")
+
+
+"""
+    Test 3 - update child row with null key, verify ok
+"""
+print("\nBegin Test 3 - update child row with null key, verify ok")
+
+child = session.query(models.Child).filter(models.Child.child_key == "c1.1").one()
+child.parent_1 = None
+session.commit()
+
+print("\n" + prt("Null parent succeeded as expected."))
+
+print("\nref_integ_tests, update completed\n\n")
+
+
+"""
+    Test 4 - update child row with new valid parent, verify ok
+"""
+test4 = True
+if test4:
+    print("\nBegin Test 4 - update child row with new valid parent, verify ok")
+    child = session.query(models.Child).filter(models.Child.child_key == "c1.1").one()
+    child.parent_1 = "p2_1"
+    child.parent_2 = "p2_2"
+
+    child.parent_1 = "p1_1"
+    child.parent_2 = "p1_2"
+
+    session.commit()
+
+    print("\n" + prt("Null parent succeeded as expected."))
+
+    print("\nref_integ_tests, update completed\n\n")
+else:
+    print("\nSKIPPED Test 4 - update child row with new valid parent, verify ok")
+
+
+"""
+    Test 5 - update parent pk, verify cascade update
+"""
+print("\nBegin Test 5 - update parent pk, verify cascade update")
+parent = session.query(models.Parent).filter(models.Parent.parent_attr_1 == "p1_1",
+                                             models.Parent.parent_attr_2 == "p1_2").one()
+parent.parent_attr_1 = "new"
+parent.parent_attr_2 = "parent"
+session.commit()  # hmm.. even with cascade all, children orphaned (!!)
+print(str(parent))
+
+child = session.query(models.Child).filter(models.Child.child_key == "c1.1").one()
+
+assert child.parent_1 == "new", "Cascade Update Failed"  # failing, pending Logic Bank RI support
+
+print("\n" + prt("parent pk updated... cascade update worked"))
+
+print("\nref_integ_tests, update completed\n\n")
+
+
+"""
+    Test 6 - delete parent row - cascade delete
+"""
+print("\nBegin Test 6 - delete parent row - cascade delete")
+parent = session.query(models.Parent).filter(models.Parent.parent_attr_1 == "p2_1",
+                                             models.Parent.parent_attr_2 == "p2_2").one()
+session.delete(parent)  # TODO - doc mass deletes don't work (query.delete())
+session.commit()  # even with cascade all, children orphaned (!!!)
+print(str(parent))
+
+children = session.query(models.Child).filter(models.Child.child_key == "c2.2").all()
+
+assert len(children)== 0, "Cascade Delete Failed"  # failing, pending Logic Bank RI support
+
+print("\n" + prt("Cascade delete succeeded as expected."))
+
+print("\nref_integ_tests, update completed\n\n")
+
+
+"""
+    Test 7 - delete parent row - cascade nullify
+"""
+print("\nBegin Test 7 - delete parent row - cascade nullify")
+
+children_orphan = session.query(models.ChildOrphan).filter(models.ChildOrphan.child_key == "c2.2").all()
+
+assert len(children_orphan) > 0, "Cascade Nullify Failed"  # failing, pending Logic Bank RI support
+
+print("\n" + prt("Cascade nullify succeeded as expected."))
+
+print("\nref_integ_tests, update completed\n\n")
+
+print("\nref_integ_tests, ran to completion\n\n")
