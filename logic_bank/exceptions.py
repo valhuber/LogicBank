f2b28fac44fc6437d66ae5afae4a11d18636e436
Thomas Pollet <thomas.pollet@gmail.com>
2024-12-27 20:36:53 +0100

    set self.invalid_rules

--- logic_bank/exceptions.py
+++ logic_bank/exceptions.py
@@ -3,6 +3,7 @@ class LBActivateException(Exception):
     message = "LogicBank Activation error"
     def __init__(self, invalid_rules = [], missing_attributes = []):
         LBActivateException = invalid_rules
+        self.invalid_rules = invalid_rules
         self.missing_attributes = missing_attributes
         self.message = f'LBActivateException: \n{LBActivateException}\n{self.missing_attributes}\n{self.message}'
         super().__init__(self.message)
